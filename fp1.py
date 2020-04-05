4ca1082c742d6093e86e09f60ec15a3fde4f603a
lamontagne-mi <55929728+lamontagne-mi@users.noreply.github.com>
2020-04-05 14:15:39 -0400

    Appends data together and adds subject dummies

--- fp1.py
+++ fp1.py
@@ -1,6 +1,18 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
-math_data = pd.read_csv("student/student-mat.csv")
+# Imports data
+math_data = pd.read_csv("student/student-mat.csv", sep = ";")
+port_data = pd.read_csv("student/student-por.csv", sep = ";")
 
-print(math_data)
+# Adds dummy variables for subject
+math_data["Math"] = [1] * len(math_data)
+math_data["Language"] = [0] * len(math_data)
+port_data["Math"] = [0] * len(port_data)
+port_data["Language"] = [1] * len(port_data)
+
+# Appends data
+student_data = pd.concat([math_data, port_data])
+
+# Reads data to new data file
+student_data.to_csv("student/student-all.csv")
